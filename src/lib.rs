c479d297cd0a3d37df0844c83e3e302b85e58012
Ville Vesilehto <ville@vesilehto.fi>
2024-11-30 22:10:14 +0200

    feat: hashset and hashmap for v2 (#16)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-//#![cfg_attr(not(feature = "std"), no_std)]
-
+// Default version is `v1` to ensure backwards compatibility
 pub use v1::{chibi_hash64, ChibiHashMap, ChibiHashSet, ChibiHasher, StreamingChibiHasher};
 
 pub mod v1;
