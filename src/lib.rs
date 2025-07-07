5eb6edee5f42e5f2c885f5bfece133274fe0f896
Alex Charlton <alex.n.charlton@gmail.com>
2025-07-07 12:08:22 -0700

    Declare no_std when not std (#19)
    * Declare no_std when not std
    
    * Fix no_std tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
 // Default version is `v1` to ensure backwards compatibility
 pub use v1::{chibi_hash64, ChibiHashMap, ChibiHashSet, ChibiHasher, StreamingChibiHasher};
 
